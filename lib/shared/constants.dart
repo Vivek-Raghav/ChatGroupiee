3710e2d3e510e3b59d8ecec1b702a2d71cd3b62a
Vivek Raghav <vivekraghva@gmail.com>
2022-12-05 16:38:28 +0530

    Changes in constants and changing my color

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class constants {
   // static String api = "";
-  final Color primaryColor = Colors.orangeAccent;
+  final Color primaryColor = Color.fromARGB(255, 123, 94, 50);
 }
